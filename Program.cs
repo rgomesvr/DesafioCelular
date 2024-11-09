6e6dd9917d1d75273a0cd7ab280f8eb2a1a492c6
rgomesvr <rslayer2015@gmail.com>
2024-11-09 04:08:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456789", "Nokia 3310", "111111111111111", 16);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Smartphone iphone = new Iphone("987654321", "iPhone X", "222222222222222", 64);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
